c3e79ef5a2e0a1984e274b29ded8b696a4d1dbf3
cyanyaan <ankitsarkar431@proton.me>
2024-08-06 22:59:52 +0530

    progress

--- main.go
+++ main.go
@@ -1,9 +1,51 @@
 package main
 
-import "flag"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	csvFilename := flag.String("csv", "problem.csv", "a file in format of 'question,answer'")
 	flag.Parse()
-	_ = csvFilename
+
+	file, err := os.Open(*csvFilename)
+
+	if err != nil {
+		exit(fmt.Sprintf("Couldn't open csv file: %s\n", *csvFilename))
+		os.Exit(1)
+	}
+	r := csv.NewReader(file)
+
+	lines, err := r.ReadAll()
+
+	if err != nil {
+		fmt.Println("Failed to parse csv file.")
+	}
+	problems := parseLines(lines)
+	fmt.Println(problems)
+}
+
+func parseLines(lines [][]string) []problem {
+	give := make([]problem, len(lines))
+
+	for i, lines := range lines {
+		give[i] = problem{
+			q: lines[0],
+			a: lines[1],
+		}
+	}
+	return give
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
